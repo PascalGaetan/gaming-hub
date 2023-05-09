4f3f91816d3ec0f0e9118d1fc41675b7faf151d2
Gaetan <lilgaetan88@gmail.com>
2023-05-09 10:29:52 -0400

    replacing chakra by bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { Grid, GridItem, Show } from '@chakra-ui/react'
-
 function App() {
 
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-    }}>
-      <GridItem pl='2' bg='orange.300' area='nav'>Nav</GridItem>
-      <Show above='lg'>
-        <GridItem pl='2' bg='gray' area='aside'>Aside</GridItem>
-      </Show>
-      <GridItem pl='2' bg='blue' area='main'>Main</GridItem>
-    </Grid>
+    <div className="btn btn-primary">add</div>
   )
 }
 
