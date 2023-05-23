05fbfd09e639b8703cc44ed3e9251c4c778f8bc6
Gaetan <lilgaetan88@gmail.com>
2023-05-23 08:17:34 -0400

    adding chakhra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import NavBar from "./components/NavBar"
+
 function App() {
 
   return (
-    <div className="btn btn-primary">add</div>
+    <div className="container">
+      <NavBar/>
+      <div className="row">
+        <div className="col-sm-4">Aside</div>
+        <div className="col-sm-8">Main</div>
+      </div>
+    </div>
   )
 }
 
